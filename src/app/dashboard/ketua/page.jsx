b614ba08cc57b2955f079faa89e5f379a4b64bdd
Guntur Bayu <email_anda@gmail.com>
2026-04-11 14:18:26 +0700

    Fix: menambah force-dynamic untuk perbaikan build

--- src/app/dashboard/ketua/page.jsx
+++ src/app/dashboard/ketua/page.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect, useMemo } from 'react';
 import { supabase } from '../../../lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export default function DashboardKetua() {
   const router = useRouter();
   
